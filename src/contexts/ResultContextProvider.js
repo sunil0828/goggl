f3259d133362c2213a5d352cae1c520e76b7aea8
Sunil L <sunill@kreaas.com>
2021-11-06 20:16:49 +0530

    final commit goggl search clone app

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setISLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("Elon Musk");
+
+  const getResults = async (type) => {
+    setISLoading(true);
+
+    const response = await fetch(`${baseUrl}${type}`, {
+      method: "GET",
+      headers: {
+        "x-user-agent": "desktop",
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(type, data);
+
+    if (type.includes("/news")) {
+      setResults(data.entries);
+    } else if (type.includes("/images")) {
+      setResults(data.image_results);
+    } else {
+      setResults(data.results);
+    }
+
+    // setResults(data);
+    setISLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
